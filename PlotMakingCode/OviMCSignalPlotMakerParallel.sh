fd95b2a61ab063491144741000fd5e8b06bd299f
Brian Calvert <BrianCalvert@Brian-Calverts-MacBook-Pro.local>
2013-07-31 23:25:03 -0400

    lots of changes, read changeLog.txt

--- PlotMakingCode/OviMCSignalPlotMakerParallel.sh
+++ PlotMakingCode/OviMCSignalPlotMakerParallel.sh
@@ -4,22 +4,22 @@ numFiles=$[numBPs+1]
 StopMassArray=($(cut -d\| -f2 StopChi0MassRunVals.txt | cut -d ' ' -f2)) #GeV
 Chi0MassArray=($(cut -d\| -f3 StopChi0MassRunVals.txt | cut -d ' ' -f2)) #GeV
 numStopMassPts=${#StopMassArray[*]}
-for mdir in `/bin/ls /data/users/bcalvert/new_oviedo_ntuples_5_14_13/*.root | grep FineBin | grep SkimOutput | sed 's/.root//g'`
+for ((startPt=1;startPt<=$numFiles;startPt++));
   do
-    echo $mdir
-    for ((startPt=1;startPt<=$numFiles;startPt++));
+  echo $startPt
+  for mdir in `/bin/ls /data/users/bcalvert/new_oviedo_ntuples_5_14_13/*.root | grep FineBin | grep SkimOutput | sed 's/.root//g'`
     do
-        echo $startPt
-	for (( c=0; c<$numStopMassPts; c++ ))
-	do
-            StopMass=${StopMassArray[$c]}
-            Chi0Mass=${Chi0MassArray[$c]}
-            echo RunStopMass $StopMass
-            echo RunChi0Mass $Chi0Mass
-            ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir isSig $StopMass $Chi0Mass doParallel $numBPs $startPt
-            ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir doPURW doHackPURW isSig $StopMass $Chi0Mass doParallel $numBPs $startPt
-        ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir doBookSyst isSig $StopMass $Chi0Mass doParallel $numBPs $startPt
-        ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir doPURW doHackPURW doBookSyst isSig $StopMass $Chi0Mass doParallel $numBPs $startPt	
-	done
+    echo $mdir
+    for (( c=0; c<$numStopMassPts; c++ ))
+      do
+      StopMass=${StopMassArray[$c]}
+      Chi0Mass=${Chi0MassArray[$c]}
+      echo RunStopMass $StopMass
+      echo RunChi0Mass $Chi0Mass
+      ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir isSig $StopMass $Chi0Mass doParallel $numBPs $startPt
+      ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir doPURW doHackPURW isSig $StopMass $Chi0Mass doParallel $numBPs $startPt
+      ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir doBookSyst isSig $StopMass $Chi0Mass doParallel $numBPs $startPt
+      ./runCondor.sh ./NewOviStopPlotFillerRunOnSkim_wSyst -i $mdir -w 0 gOutDir doPURW doHackPURW doBookSyst isSig $StopMass $Chi0Mass doParallel $numBPs $startPt	
     done
+  done
 done
